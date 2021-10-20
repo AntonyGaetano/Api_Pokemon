728b1dfe98196e71617041eaa08ba5197ef451d6
Antony Gaetano <86926752+AntonyGaetano@users.noreply.github.com>
2021-10-20 13:07:12 -0300

    ...

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 
 
-/*
+
 var url = "https://pokeapi.co/api/v2/pokemon/pikachu/"
 fetch(url)
 .then(response => response.json())
 .then(data=>{
-    document.write(data.name)
-    document.write("<br>")
-    document.write(data.order)
-    document.write(`<img src='${data.sprites.front_default}'/>`)
+    //document.write(data.name)
+    //document.write("<br>")
+    //document.write(data.order)
+    //document.write(`<img src='${data.sprites.front_default}'/>`)
     console.log(data)
-}) */
+}) 
